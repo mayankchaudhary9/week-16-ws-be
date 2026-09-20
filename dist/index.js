851ed22ae543b770d028aee0dfc763ec7127389b
mayankchaudhary9 <chaudharymayank5800@gmail.com>
2026-09-20 22:23:42 +0530

    Initial commit

---
+++ dist/index.js
@@ -0,0 +1,17 @@
+import { WebSocketServer } from 'ws';
+const wss = new WebSocketServer({ port: 8080 });
+// event handler
+wss.on("connection", function (socket) {
+    console.log("A new client has connected.");
+    // setInterval(() => {
+    //     socket.send("Welcome to the WebSocket server!");
+    // }, 500);
+    socket.on("message", (e) => {
+        console.log(e.toString());
+        console.log(e.toString() === "ping");
+        if (e.toString() === "ping") {
+            socket.send("pong");
+        }
+    });
+});
+//# sourceMappingURL=index.js.map
